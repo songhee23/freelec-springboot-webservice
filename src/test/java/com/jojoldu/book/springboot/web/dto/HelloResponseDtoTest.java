3a0e15fdce160b653ed7f8defc1ca9d60be96f01
songhee27 <songhee27@ajpark.co.kr>
2021-11-17 18:43:44 +0900

    assertj

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,7 +1,8 @@
 package com.jojoldu.book.springboot.web.dto;
 
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;  // main one
+
 
 public class HelloResponseDtoTest {
 
@@ -17,6 +18,26 @@ public class HelloResponseDtoTest {
         // then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
-
+        // assertThat(actual).isEqualTo(Expected);
+        // 왼쪽에서 오른쪽으로 자연스럽게 읽힌다.
+        /* 1. assertJ
+        - assertJ라는 테스트 검증 라이브러리의 검증 메소드입니다.
+        - 검증하고 싶은 대상을 메소드 인자로 받습니다.
+        - 메소드 체이닝이 지원되어 isEqualTo와 같이 메소드를 이어서 사용할 수 있습니다.
+        * */
     }
+
+    // assertJ 테스트 참조
+    // https://pjh3749.tistory.com/241
+
+    @Test
+    public void a_few_simple_assertions() {
+        assertThat("The Lord of the Rings")
+                .isNotNull()
+                .startsWith("The")
+                .contains("Lord")
+                .endsWith("Rings");
+    }
+
+
 }
