889276ed3c53e44586f027811a1cecc85ad83697
songhee27 <songhee27@ajpark.co.kr>
2021-11-26 11:19:30 +0900

    jUnit테스트

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,4 +18,13 @@ public class HelloController {
     }
     /* @GetMapping HTTP Method인 Get요청을 받을 수 있는 API를 만들어 줍니다.
     * */
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        /* @RequestParam
+        - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션입니다.
+        - 여기서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메소드 파라비터 name(String name) 에 저장하게 됩니다.
+         * */
+        return new HelloResponseDto(name, amount);
+    }
 }
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -39,5 +39,4 @@ public class HelloResponseDtoTest {
                 .endsWith("Rings");
     }
 
-
 }
